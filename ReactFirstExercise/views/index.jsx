661f4a66b6520e27be682b0ebad29ef93714f3ab
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-31 08:55:33 +0100

    Completed challenge

--- ReactFirstExercise/views/index.jsx
+++ ReactFirstExercise/views/index.jsx
@@ -1,40 +1,34 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+ import React from 'react';
 
-export default class TodoBox extends React.Component {
-  // Omitted
-}
-
-class TodoList extends React.Component {
-    render() {
-        return (
-            <div className="todoList">
-                <table style={{border: "2px solid black"}}>    
-                  <tbody>
-                    <Todo title="Shopping">Milk</Todo>
-                    <Todo title="Hair cut">13:00</Todo>        
-                    <Todo title="Learn React">15:00</Todo>     
-                  </tbody>
-                </table>
-            </div>
-        );
+    export default class TodoBox extends React.Component {
+      // Omitted
     }
-}
 
-class Todo extends React.Component {
-    render() {
-        return (
-            <tr>
-                <td style={{border: "1px solid black"}}>{this.props.title}</td>
-                <td style={{border: "1px solid black"}}>{this.props.children}</td>
-            </tr>
-        );
+    class TodoList extends React.Component {
+      // Omitted
     }
-}
-Todo.propTypes = {
-    title: PropTypes.string.isRequired
-};
 
-class TodoForm extends React.Component {
-  // Omitted
-}
+    class Todo extends React.Component {
+        constructor(props) {
+            super(props);
+        }
+
+        render() {
+            return (
+                <tr>
+                    <td style={{border: "1px solid black"}}>
+                        <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
+                    </td>
+                    <td style={{border: "1px solid black"}}>{this.props.title}</td>      
+                    <td style={{border: "1px solid black"}}>{this.props.children}</td>   
+                </tr>
+            );
+        }
+    }
+    Todo.propTypes = {
+        title: React.PropTypes.string.isRequired
+    };
+
+    class TodoForm extends React.Component {
+      // Omitted
+    }
