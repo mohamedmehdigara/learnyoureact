fb46e07096f05885037985a17d13b0b91a671876
Mohamed-Mehdi-Gara <garamehdi57@yahoo.com>
2022-01-31 09:00:02 +0100

    Completed challenge

--- ReactFirstExercise/views/index.jsx
+++ ReactFirstExercise/views/index.jsx
@@ -1,34 +1,67 @@
- import React from 'react';
+import React from 'react';
 
-    export default class TodoBox extends React.Component {
-      // Omitted
-    }
+export default class TodoBox extends React.Component {
+    render() {
+    return (
+      <div className="todoBox">
+        <h1>Todos</h1>
+        <TodoList data = {this.props.data} />
+        <TodoForm />
+      </div>
+    );
+  }
+}
 
-    class TodoList extends React.Component {
-      // Omitted
-    }
+class TodoList extends React.Component {
+    render() {
+    var todo = this.props.data.map(function(obj) { return <Todo title={obj.title} key={obj.title}>{obj.detail}</Todo>});
+    return (
+      <div className = "todoList">
+        <table style={{border: "2px solid black"}}>
+          <tbody>
+            {todo}
+          </tbody>
+        </table>
+      </div>
+    );
+  }
+}
 
-    class Todo extends React.Component {
-        constructor(props) {
-            super(props);
-        }
+class Todo extends React.Component {
+  // Omitted
+}
+Todo.propTypes = {
+  // Omitted
+};
 
-        render() {
-            return (
-                <tr>
-                    <td style={{border: "1px solid black"}}>
-                        <input type="checkbox" checked={this.state.checked} onChange={this.handleChange.bind(this)}/>
-                    </td>
-                    <td style={{border: "1px solid black"}}>{this.props.title}</td>      
-                    <td style={{border: "1px solid black"}}>{this.props.children}</td>   
-                </tr>
-            );
-        }
-    }
-    Todo.propTypes = {
-        title: React.PropTypes.string.isRequired
-    };
+class TodoForm extends React.Component {
+  // Omitted
+}
 
-    class TodoForm extends React.Component {
-      // Omitted
-    }
+let style = {
+  // Omitted
+};
+
+Next, we'll change the code on our server, program.js. Specifically, we'll change the    
+callback function of app.use() and pass it a data variable.
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.argv[2] || 3000));
+app.set('view engine', 'jsx');
+app.set('views', __dirname + '/views');
+app.engine('jsx', require('express-react-views').createEngine({ transformViews: false }));
+
+require('babel/register')({
+    ignore: false
+});
+
+// write below
+var data = [];
+
+app.use('/', function(req, res) {
+  res.render('index', {data: data});
+});
+
+app.listen(app.get('port'), function() {});
